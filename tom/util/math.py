53686215d82a9ded30e9640c370bf85f0c9785a7
ThatDevShparki <matt.murray@r2c.dev>
2023-03-03 09:54:03 -0600

    chore(tom): ♻️ Maths types

--- tom/util/math.py
+++ tom/util/math.py
@@ -8,28 +8,31 @@ import numpy.typing as npt
 
 # Types
 
+Int = np.int_ | int
+Float = np.float_ | float
+
 NDArrayInt = npt.NDArray[np.int_]
 NDArrayFloat = npt.NDArray[np.float_]
 NDArray = NDArrayInt | NDArrayFloat
 
 
-def count_graphs(n: int) -> float:
+def count_graphs(n: Int) -> Float:
     edges = n * (n - 1) / 2
     return 2**edges
 
 
-def binomial_coeff(n: int, k: int) -> float:
-    return math.factorial(n) / (math.factorial(n - k))
+def binomial_coeff(n: Int, k: Int) -> Float:
+    return np.divide(math.factorial(n), (math.factorial(n - k)))
 
 
-def distance_from_origin(x: int, y: int) -> float:
+def distance_from_origin(x: Float, y: Float) -> Float:
     return np.sqrt(x**2 + y**2)
 
 
-def generate_binary_strings(bit_count: int) -> list[str]:
+def generate_binary_strings(bit_count: Int) -> list[str]:
     binary_strings: list[str] = []
 
-    def genbin(n: int, bs: str = "") -> None:
+    def genbin(n: Int, bs: str = "") -> None:
         if len(bs) == n:
             binary_strings.append(bs)
         else:
@@ -40,14 +43,14 @@ def generate_binary_strings(bit_count: int) -> list[str]:
     return binary_strings
 
 
-def cycle_list(loop: list[t.Any], loops: int = 1):
+def cycle_list(loop: list[t.Any], loops: Int = 1):
     n = len(loop)
     for _t in range(loops):
         loop = [loop[(i + 1) % n] for i in range(n)]
     return loop
 
 
-def generate_unique_combinations(n: int):
+def generate_unique_combinations(n: Int) -> list[list[Int]]:
     combinations = generate_binary_strings(n)
     non_repeating = [combinations[0]]
     for i in range(len(combinations)):
@@ -62,6 +65,5 @@ def generate_unique_combinations(n: int):
         if test == 0:
             non_repeating.append(combinations[i])
 
-    for i in range(len(non_repeating)):
-        non_repeating[i] = [int(s) for s in list(non_repeating[i])]
-    return non_repeating
+    int_non_repeating: list[list[Int]] = [[int(s) for s in nr] for nr in non_repeating]
+    return int_non_repeating
